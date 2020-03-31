404bcacb08bdc18b0e39f4552c95f5588be8505a
Vandna Garg <vandna.garg@nagarro.com>
2020-03-31 12:55:39 +0530

    Kubernet first commit

---
+++ UserAPI/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace UserAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<object> Get()
+        {
+            var response =  "{\"name\": \"John\",  \"age\": \"23\",\"email\": \"john.doe@google.com\"}";
+            var result = JsonConvert.DeserializeObject<object>(response);
+            return result;
+        }
+    }
+}
